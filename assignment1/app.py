9a40762175b4b7c9fa5e3573b1859a21d55a8ebd
Sam Vredenburgh <sam.vburgh@gmail.com>
2025-06-23 21:44:09 -0600

    basic assignment1 functionality

--- assignment1/app.py
+++ assignment1/app.py
@@ -8,6 +8,20 @@ st.markdown("This app will load a sentiment analysis classification model, allow
 # Step 3: Load the Saved Model
 @st.cache_data
 def load_model():
-    """Loads the pre-trained model and target names."""
+    """Loads the pre-trained model."""
     model = joblib.load('sentiment_model.pkl')
     return model
+
+model = load_model()
+
+# Step 4: Create the User Input Interface
+user_text = st.text_area("Enter a movie review to analyze:")
+analyze = st.button("Analyze")
+
+# Step 5: Make Predictions and Display Results
+if analyze:
+    if len(user_text) > 0:
+        output = model.predict([user_text])
+        st.subheader(output)
+        proba = model.predict_proba([user_text])
+        st.subheader(proba)
